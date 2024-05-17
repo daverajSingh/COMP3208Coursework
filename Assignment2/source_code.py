99c7916ad63b24dfaff910d6f6c4f94eacf2fb2b
daveraj <daveraj.singh@outlook.com>
2024-05-17 01:19:05 +0100

    Added Database Functionality

--- Assignment2/source_code.py
+++ Assignment2/source_code.py
@@ -4,43 +4,59 @@
 # algorithm to train and then predict ratings for a large (20M) set of items.
 
 import numpy as np
-# File Handling
-def loadData(file):
-    return np.genfromtxt(file, delimiter=',', dtype=np.float64)
+import sqlite3
 
-def saveData(file, data):
-    np.savetxt(file, data, delimiter=',')
+#Setup the database
 
-def matrixToCSV(data, timestamps):
-    rows, cols = data.shape
-    dataCSV = []
-    for i in range(rows):
-        for j in range(cols):
-            if(data[i, j] != 0):
-                dataCSV.append([i+1, j+1, data[i, j], timestamps[i, j]])
-    return np.array(dataCSV)
+def setupDatabase(dbFile):
+    """
+    Set up the database with the given schema.
+    
+    returns:
+    conn: connection to the database
+    """
+    conn = sqlite3.connect(dbFile)
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS train (userId INT, itemId INT, rating REAL, timestamp INT)')
+    c.execute('CREATE TABLE IF NOT EXISTS test (userId INT, itemId INT, timestamp INT)')
+    conn.commit()
+    return conn
 
-# Matrices
-def timestampMatrix(data, users, items):
-    timestamps = np.zeros((users, items))
-    for row in data:
-        timestamps[int(row[0])-1, int(row[1])-1] = row[3].astype(np.uint64)
-    return timestamps
 
-def userItemMatrix(data, users, items):
+def loadData(file, conn):
+    """
+    Load data to the database.
+    
+    args:
+    dbFile: path to the database file
+    """
+    
+    c = conn.cursor()
+    with open(file, 'r') as f:
+        for line in f:
+            line = line.strip().split(',')
+            if len(line) == 4:
+                c.execute('INSERT INTO train VALUES (?, ?, ?, ?)', (line[0], line[1], line[2], line[3]))
+            elif len(line) == 3:
+                c.execute('INSERT INTO test VALUES (?, ?, ?)', (line[0], line[1], line[2]))
+    conn.commit()
+    
+
+def userItemMatrix(conn):
+    c = conn.cursor()
+    users, items = getMaxUserItemId(conn)
     userItemMatrix = np.zeros((users, items))
-    for row in data:
-        userItemMatrix[int(row[0])-1, int(row[1])-1] = row[2].astype(np.float64)
+    for row in c.execute('SELECT * FROM train'):
+        userItemMatrix[int(row[0])-1, int(row[1])-1] = row[2]
     return userItemMatrix
-
+    
 # Matrix Factorisation
-def matrixFactorisation(userItemMatrix, k, learningRate, iterations, regularisation):
+def matrixFactorisation(userItemMatrix, learningRate, iterations, regularisation, NFactors=40):
     """
     Performs matrix factorisation using stochastic gradient descent.
     
     args:
     userItemMatrix: matrix with users as rows and items as columns
-    k: number of latent features
     learningRate: learning rate
     iterations: number of iterations
     regularisation: regularisation parameter
@@ -49,90 +65,66 @@ def matrixFactorisation(userItemMatrix, k, learningRate, iterations, regularisat
     P: user matrix
     Q: item matrix
     """
-    N = len(userItemMatrix)
-    M = len(userItemMatrix[0])
+    nUsers, nItems = userItemMatrix.shape
     
     # Randomly initialize user and item matrices
-    P = np.random.rand(N, k)
-    Q = np.random.rand(M, k)
-    
-    # Transpose Q for easier dot product calculation
-    Q = Q.T
+    q = np.random.rand(nItems, NFactors)
+    p = np.random.rand(nUsers, NFactors)
     
     # SGD for given number of steps
     for step in range(iterations):
-        for u in range(N):
-            for i in range(M):
-                if userItemMatrix[u, i] > 0:  # Only consider non-zero ratings
-                    eui = userItemMatrix[u, i] - np.dot(P[u, :], Q[:, i])
-                    for t in range(k):
-                        P[u, t] += learningRate * (2 * eui * Q[k, i] - regularisation * P[u, t])
-                        Q[t, i] += learningRate * (2 * eui * P[u, k] - regularisation * Q[t, i])
-        
-        # Calculate the error
-        e = 0
-        for u in range(N):
-            for i in range(M):
-                if userItemMatrix[u, i] > 0:
-                    e += pow(userItemMatrix[u, i] - np.dot(P[u, :], Q[:, i]), 2)
-                    for t in range(k):
-                        e += (regularisation / 2) * (pow(P[u, t], 2) + pow(Q[t, i], 2))
-        
-        if e < 0.001:
-            break
-    
-    return P, Q.T
+        # Make a randomly shuffled list of user-item pairs
+        indices = np.arange(nUsers)
+        np.random.shuffle(indices)
+        #Loop on each user-item pair
+        for i in indices:
+            for j in range(nItems):
+                if userItemMatrix[i, j] > 0:
+                    predictedRating = np.dot(p[i, :], q[j, :])
+                    # Calculate the error
+                    error = userItemMatrix[i, j] - predictedRating
+                    # Update the user and item matrices
+                    p[i, :] += learningRate * (error * q[j, :] - regularisation * p[i, :])
+                    q[j, :] += learningRate * (error * p[i, :] - regularisation * q[j, :])
+        print("Iteration", step, " - Error = ", error) # Print the error for each iteration
+    return p, q
 
-def getPredictions(predictedRatings, testData):
-    """
-    Get the predicted ratings for the test data.
+
+def getPredictions(P, Q, conn):
+    Q = Q.T
+    predictedRatings = np.dot(P, Q)
+    c = conn.cursor()
+    for row in c.execute("SELECT userId, itemId, timestamp FROM test"):
+        userId = int(row[0]) - 1
+        itemId = int(row[1]) - 1
+        timestamp = row[2]
+        predictedRatings.append((userId, itemId, predictedRatings[userId, itemId], timestamp))
     
-    args:
-    predictedRatings: matrix with predicted ratings
-    testData: test data
+    np.savetxt('output.csv', predictedRatings, delimiter=',')
+
+def getMaxUserItemId(conn):
+    c = conn.cursor()
+    maxTrainUser = c.execute('SELECT MAX(userId) FROM train').fetchone()[0]
+    maxTestUser = c.execute('SELECT MAX(userId) FROM test').fetchone()[0]
+    maxTrainItem = c.execute('SELECT MAX(itemId) FROM train').fetchone()[0]
+    maxTestItem = c.execute('SELECT MAX(itemId) FROM test').fetchone()[0]
+    print(maxTrainUser, maxTestUser, maxTrainItem, maxTestItem)
+    return max(maxTrainUser, maxTestUser), max(maxTrainItem, maxTestItem)   
     
-    returns:
-    ratings: predicted ratings for the test data
-    """
-    ratings = np.zeros(testData.shape)
-    for row in testData:
-        ratings[row[0], row[1]] = predictedRatings[row[0]-1, row[1]-1]
-    return ratings
 
 print("Loading data...")
-
-train = loadData('Assignment2/train_20M_withratings.csv')
-test = loadData('Assignment2/test_20M_withoutratings.csv')
-
-print("Data loaded.")
-
-usersTrain = int(np.max(train[:, 0]))
-itemsTrain = int(np.max(train[:, 1]))
-usersTest = int(np.max(test[:, 0]))
-itemsTest = int(np.max(test[:, 1]))
-totalUsers = max(usersTrain, usersTest)
-totalItems = max(itemsTrain, itemsTest)
-
-print("Making matrices...")
-
-userItemMatrixTrain = userItemMatrix(train, totalUsers, totalItems)
-timestamps = timestampMatrix(train, totalUsers, totalItems)
-
-print("Matrices made.")
+conn = setupDatabase('work.db')
+print("Database setup.")
+loadData('test_20M_withoutratings.csv', conn)
+print("Test data loaded.")
+loadData('train_20M_withratings.csv', conn)
+print("Train data loaded.")
+print("Making User Item Matrix...")
+userItemMatrixTrain = userItemMatrix(conn)
+print("Matrix made.")
 print("Starting matrix factorisation...")
-
-P, Q = matrixFactorisation(userItemMatrixTrain,)
-
+P, Q = matrixFactorisation(userItemMatrixTrain, learningRate=0.1, iterations=10, regularisation=0.0)
 print("Matrix factorisation done.")
 print("Fetching ratings...")
-
-predictedRatings = np.dot(P, Q.T)
-predictedRatings = getPredictions(predictedRatings, test)
-
-print("Ratings fetched.")
-print("Saving data...")
-
-ratings = matrixToCSV(predictedRatings, timestamps)
-saveData('output.csv', ratings)
-
+getPredictions(P, Q, conn)
 print("Data saved.")
